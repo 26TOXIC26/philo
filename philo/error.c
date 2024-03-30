7c6760c068172c9b748b6b1ca495254ae69d6374
26toxic26 <bdmousaid@gmail.com>
2024-03-30 03:26:22 +0000

    2

--- philo/error.c
+++ philo/error.c
@@ -6,7 +6,7 @@
 /*   By: pc <pc@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 20:24:39 by amousaid          #+#    #+#             */
-/*   Updated: 2024/03/28 11:08:07 by pc               ###   ########.fr       */
+/*   Updated: 2024/03/30 03:25:21 by pc               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	is_error(void)
 {
-	printf("Error\n");
+	printf("Error in arg\n");
+	printf("--->your input most be positive and ofcourse most be number (number!!!!!!)\n");
+	printf("--->and you can put 4 or 5 arg not more or less then\n");
 	exit(0);
 }
